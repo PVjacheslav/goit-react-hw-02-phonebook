764a0e35c69c75ce7ee171bcd17777677f2e4a92
PVjacheslav <128468517+PVjacheslav@users.noreply.github.com>
2023-10-15 14:02:11 +0300

    develop task-2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App/App';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'components/GlobalStyle';
+
+const theme = {
+  colors: {
+    yellow: '#F0F011',
+    blue: '#2450DB',
+    error: 'red',
+  },
+  spacing: value => `${value * 4}px`,
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={{ theme }}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>
 );
